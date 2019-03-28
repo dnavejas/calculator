431235d0ab3dee5203756b551f214748979be57a
Danny Navejas <dannynavejas@gmail.com>
2019-03-28 17:41:30 -0500

    added bootstrap styling and transferred js code to calculator.js file

---
+++ calculator.js
@@ -0,0 +1,13 @@
+var calculator = new Object();
+calculator.add = function (x, y){
+    return x + y;
+}
+calculator.subtract = function (x, y){
+    return x - y;
+}
+calculator.divide = function (x, y){
+    return x / y;
+}
+calculator.multiply = function (x, y) {
+    return x * y;
+}
