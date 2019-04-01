750ebe2f5453f8430d068cb6bf00e3f663bb0bd1
Danny Navejas <dannynavejas@gmail.com>
2019-04-01 18:16:28 -0500

    added drakes solution which is commented out

--- calculator.js
+++ calculator.js
@@ -1,13 +1,131 @@
-var calculator = new Object();
-calculator.add = function (x, y){
-    return x + y;
-}
-calculator.subtract = function (x, y){
-    return x - y;
-}
-calculator.divide = function (x, y){
-    return x / y;
-}
-calculator.multiply = function (x, y) {
-    return x * y;
-}
+// var calculator = new Object();
+// calculator.add = function (x, y){
+//     return x + y;
+// }
+// calculator.subtract = function (x, y){
+//     return x - y;
+// }
+// calculator.divide = function (x, y){
+//     return x / y;
+// }
+// calculator.multiply = function (x, y) {
+//     return x * y;
+// }
+// $("button").click(function() {
+//     var fired_button = $(this).val();
+//     alert(fired_button);
+// });
+
+
+// ----------------------------- Drake Siemer √√√√√√√√
+
+// function splitValue(value, index) {
+//     return value.substring(0, index) + "," + value.substring(index);
+// }
+
+// const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// function divide (x,y) {
+//     return parseInt(x) / parseInt(y);
+// }
+
+// function subtract (x,y) {
+//     return parseInt(x) - parseInt(y);
+// }
+
+// function multiply(x,y) {
+//     return parseInt(x) * parseInt(y);
+// }
+
+// function add(x, y) {
+//     return parseInt(x) + parseInt(y);
+// }
+
+
+// function cal (expression) {
+//     // check for +-/*
+//     const regex = /[0-9]/g;
+
+//     let x_value = [];
+//     let y_value = [];
+//     let operator;
+
+//     let operators = expression.replace(regex,' ').split(' ').filter((val) => !!val);
+
+//     let pushToXorY = true; 
+
+//     if (operators.length !== 0 && operators.length === 1) {
+//         let returnedValue;
+
+//         let values = expression.split(operators[0]);
+    
+//         if (operators[0] === "+") {
+
+//             returnedValue = add(values[0], values[1])
+//         } else if (operators[0] === "-") {
+
+//             returnedValue = subtract(values[0], values[1])
+//         } else if (operators[0] === "*") {
+
+//             returnedValue = multiply(values[0], values[1])
+//         } else if (operators[0] === "/") {
+
+//             returnedValue = divide(values[0], values[1])
+//         }
+
+//         $("#display").val(returnedValue)
+//     } else {
+//         for(let i = 0; i < expression.length; i++) {
+//             // if i is not a +-/* check which array to number to
+//             // else 
+//             if (expression[i] !== "*" && expression[i] !== "+" && expression[i] !== "-" && expression[i] !== "/") {
+//                 if (pushToXorY === true) {
+//                     x_value.push(expression[i]);
+//                 } else {
+//                     y_value.push(expression[i])
+//                 }
+//             } else {
+//                 if (x_value.length !== 0 && y_value.length !== 0) {
+//                     let returnedValue;
+    
+//                     if (operator === "+") {
+    
+//                         returnedValue = add(x_value.reduce(reducer), y_value.reduce(reducer)) + expression.substring(i);
+//                     } else if (operator === "-") {
+    
+//                         returnedValue = subtract(x_value.reduce(reducer), y_value.reduce(reducer)) + expression.substring(i);
+//                     } else if (operator === "*") {
+    
+//                         returnedValue = multiply(x_value.reduce(reducer), y_value.reduce(reducer)) + expression.substring(i);
+//                     } else if (operator === "/") {
+    
+//                         returnedValue = divide(x_value.reduce(reducer), y_value.reduce(reducer)) + expression.substring(i);
+//                     }
+    
+//                     if (!!returnedValue) {
+//                         cal(returnedValue)
+//                     }
+//                 }
+    
+//                 operator = expression[i]
+//                 pushToXorY = false;
+//             }
+//         }
+//     }
+// }
+
+
+// $(":button").click(function() {
+
+//     let main_input = $("#display");
+
+//     let fired_button = $(this).val();
+
+//     if (fired_button !== "=") {
+//         main_input.val(main_input.val() + fired_button);
+//     } else {
+//         cal(main_input.val())
+//     }
+// });
+
+// -----------------------------
